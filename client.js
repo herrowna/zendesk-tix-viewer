5691daa623fa1ed34df9f261fb0079ef31c4130a
Helena Ng <hel@love.com>
2017-07-15 17:46:44 +1000

    Changed socket.io events, next_page link now handled by server.

--- client.js
+++ client.js
@@ -2,14 +2,27 @@ $( document ).ready(function() {
     var socket = io.connect();
 
     socket.on('connect', function(data) {
-    socket.emit('join');
+        socket.emit('join');
+    });
+
+    socket.on('errorFromProvider', function(data){
+        $('#status').html(data)
     })
 
-    socket.on('ticketsReceived', function(data){
+    var populate_tickets_list = function(data){
         $('#status').html(data.count +' tickets in total.')
         var tickets = data['tickets']   // new variable here just to shorten the JSON object query
         for (var i in tickets){ 
             $('#tickets_list').append('<li>'+'<a href="'+tickets[i].url+'">'+tickets[i].subject+'</a>'+'</li>')
         }
-    })
+    };
+
+    socket.on('ticketsReceived', function(data){
+        populate_tickets_list(data)
+        $('#next_page').click(function(){
+            socket.emit('hasNextPage', page_link)
+            })
+    });
+
+
 });
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+'use strict'
 // config me
 var client_configuration = {mimetypes: {json: ["application/json", "application/json;charset=utf-8"]}, 
                             user: "hel@love.com",   
@@ -9,26 +10,56 @@ var app = express();
 app.use(express.static(__dirname + ''));
 app.get('/', function(req, res) {  
         res.sendFile(__dirname + '/index.html');
-});
+})
 var server = require('http').createServer(app);  
 var io = require('socket.io')(server);
 var Client = require('node-rest-client').Client;
-var client = new Client(client_configuration);
+var rest_client = new Client(client_configuration);
 
-var get_all_tickets = function(){
-    var args = {path: { "subdomain":"acmecorp", "item":"tickets", "per_page": 25 }};
-    client.get("https://${subdomain}.zendesk.com/api/v2/${item}.json?per_page=${per_page}", args, function (data, response) {
-    io.emit('ticketsReceived', data);
-    });
-};
+var check_next_page = function(data){
+    var next_page_link = data['next_page'];
+    try {
+        if('next_page' in data == null) {
+            throw new IndexError('Reached end of query')
+        }
+        console.log('More tickets found');
+        io.emit('hasNextPage', next_page_link)
+        
+    } catch (e) {
+        console.log(e.message);
+    }
+}
 
+var get_tickets = function(url){
+    rest_client.get(url, function(data, response){
+        try {
+            if('error' in data) {
+                var error = JSON.stringify(data['error'])
+                io.emit('errorFromProvider', error);
+                throw new SyntaxError(error);
+            } else {
+                io.emit('ticketsReceived', data);
+                check_next_page(data)
+            }
+        } catch(e) {
+            console.log(JSON.stringify(e.message));
+        } 
+
+    }).on('error', function(err){
+        console.log('Something went wrong on the request', err.request.options);
+    })
+}
 
 io.on('connection', function(client) {
+    get_tickets('https://acmecorp.zendesk.com/api/v2/tickets.json?&per_page=25');
     client.on('join', function(data) {
-    console.log('Client connected...');
-    get_all_tickets();
+        console.log('Client connected...');
     })
-});
+
+    client.on('getMoreTickets', function(data){
+        get_tickets(data['next_page'])
+    })
+})
 
 server.listen(port);  
 console.log('Server available at http://localhost:' + port);
